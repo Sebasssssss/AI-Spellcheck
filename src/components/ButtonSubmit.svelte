607217cddaeccb08655564cb3f732b094939d5c8
Sebasssssss <sebastian.rodriguez9714@gmail.com>
2023-05-01 16:54:24 -0300

    First commit

---
+++ src/components/ButtonSubmit.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { fixMyEnglish } from '../services/ia.js'
+  import Loading from './Icons/Loading.svelte'
+  import Upload from './Icons/Upload.svelte'
+  let promise = null
+
+  const handleClick = async () => {
+    const text = document.getElementById("result").value
+    promise = fixMyEnglish(text)
+    const value = await promise
+
+    document.getElementById("result").value = value
+    promise = null
+  }
+
+</script>
+
+{#if promise === null}
+
+  <button on:click={handleClick} type="button" class="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+      <Upload /> Fix it
+  </button>
+
+{:else}
+
+  {#await promise}
+
+      <button type="button" class="pointer-events-none py-2 px-4 flex justify-center items-center  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+        <Loading /> Loading...
+      </button>
+
+  {/await}
+
+
+{/if}
---
+++ src/services/ia.js
@@ -0,0 +1,44 @@
+const COHERE_API_KEY = 'JvX596EW4CCilp6CpnoelBPfGA8QAxngUWIzyUCe'
+const COHERE_API_GENERATE_URL = 'https://api.cohere.ai/generate'
+const COHERE_API_DETECT_LANGUAGE_URL = 'https://api.cohere.ai/detect-language'
+
+export async function fixMyEnglish(input) {
+  const data = {
+    model: 'xlarge',
+    prompt: `This is a spell checker generator.
+    --
+    Incorrect sample: "I are good!"
+    Correct sample: "I am good!"
+    --
+    Incorrect sample: "I have 22 years old."
+    Correct sample: "I am 22 years old."
+    --
+    Incorrect sample: "I don't can know."
+    Correct sample: "I don't know."
+    --
+    Incorrect sample: "${input}"
+    Correct sample:`,
+    max_tokens: 40,
+    temperature: 0.3,
+    k: 0,
+    p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stop_sequences: ['--'],
+    return_likelihoods: 'NONE'
+  }
+
+  const response = await fetch(COHERE_API_GENERATE_URL, {
+    method: 'POST',
+    headers: {
+      Authorization: `BEARER ${COHERE_API_KEY}`,
+      'Content-Type': 'application/json',
+      'Cohere-Version': '2022-12-06'
+    },
+    body: JSON.stringify(data)
+  }).then(res => res.json())
+
+  const { text } = response.generations[0]
+
+  return text.replace('--', '').replaceAll('"', '').trim()
+}
