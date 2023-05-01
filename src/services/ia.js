0900e410ba45da162f4c7a97bf9b3e7d6bb21cdc
Sebasssssss <sebastian.rodriguez9714@gmail.com>
2023-05-01 17:38:14 -0300

    Updates

--- src/services/ia.js
+++ src/services/ia.js
@@ -1,4 +1,4 @@
-const COHERE_API_KEY = 'JvX596EW4CCilp6CpnoelBPfGA8QAxngUWIzyUCe'
+const COHERE_API_KEY = 'YOUR_API_KEY'
 const COHERE_API_GENERATE_URL = 'https://api.cohere.ai/generate'
 const COHERE_API_DETECT_LANGUAGE_URL = 'https://api.cohere.ai/detect-language'
 
